96333c67d2a2364e9d2ed5eb28fd633b0521ff42
Claudio Plaza <plza0000@gmail.com>
2022-12-14 00:10:36 -0300

    Creando Tarjetas Card Widget

--- lib/screens/card_screen.dart
+++ lib/screens/card_screen.dart
@@ -1,14 +1,34 @@
 import 'package:flutter/material.dart';
 
+import 'package:fl_components/theme/app_theme.dart';
+
 class CardScreen extends StatelessWidget {
   const CardScreen({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Center(
-        child: Text('CardScreen'),
-      ),
-    );
+    return Scaffold(
+        appBar: AppBar(
+          title: Text('Card Widget'),
+        ),
+        body: ListView(
+          padding: const EdgeInsets.symmetric(horizontal: 10, vertical: 10),
+          children: [
+            Card(
+                child: Column(
+              children: [
+                ListTile(
+                  leading: Icon(
+                    Icons.add_a_photo_outlined,
+                    color: AppTheme.primary,
+                  ),
+                  title: Text('Titulo'),
+                  subtitle: Text(
+                      'esto es un parrafo sobre card una screen de mi aplicacion componentes'),
+                )
+              ],
+            ))
+          ],
+        ));
   }
 }
