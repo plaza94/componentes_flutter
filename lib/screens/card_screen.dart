2fef6b8be47469ad6ff46b16940833b4674329d4
Claudio Plaza <plza0000@gmail.com>
2022-12-15 12:08:58 -0300

    Cambio a targetas con url y titulo dinamico

--- lib/screens/card_screen.dart
+++ lib/screens/card_screen.dart
@@ -16,11 +16,21 @@ class CardScreen extends StatelessWidget {
           children: const [
             CustomCardType1(),
             SizedBox(height: 20),
-            CustomCardType2(),
+            CustomCardType2(
+              imageURL:
+                  'https://i.pinimg.com/564x/b7/e2/66/b7e266025c0484e2c22f60e049aa7aac.jpg',
+              name: 'Tunning Car Pinterest',
+            ),
             SizedBox(height: 20),
-            CustomCardType2(),
+            CustomCardType2(
+              imageURL:
+                  'https://i.pinimg.com/564x/04/bb/cf/04bbcfaa24a90b9e04f26e600616a4cb.jpg',
+            ),
             SizedBox(height: 20),
-            CustomCardType2(),
+            CustomCardType2(
+              imageURL:
+                  'https://i.pinimg.com/564x/f2/a7/45/f2a745579356f8dd5246a1180c9ecde8.jpg',
+            ),
             SizedBox(height: 100),
           ],
         ));
--- lib/widgets/custom_card_type_2.dart
+++ lib/widgets/custom_card_type_2.dart
@@ -2,7 +2,10 @@ import 'package:fl_components/theme/app_theme.dart';
 import 'package:flutter/material.dart';
 
 class CustomCardType2 extends StatelessWidget {
-  const CustomCardType2({super.key});
+  final String imageURL;
+  final String? name;
+
+  const CustomCardType2({super.key, required this.imageURL, this.name});
 
   @override
   Widget build(BuildContext context) {
@@ -15,26 +18,27 @@ class CustomCardType2 extends StatelessWidget {
           0.5), //cambia color de la sombra y la opacidad de esta misma
       child: Column(
         children: [
-          const FadeInImage(
-            image: NetworkImage(
-                'https://i.pinimg.com/564x/b7/e2/66/b7e266025c0484e2c22f60e049aa7aac.jpg'),
-            placeholder: AssetImage(
+          FadeInImage(
+            image: NetworkImage(imageURL),
+            placeholder: const AssetImage(
                 'assets/jar-loading-dark.gif'), //imagen cuando esta aun no carga (gif)
             width: double
                 .infinity, //que el ancho sea completo osea del anchio de la tarjeta
             height: 230,
             fit: BoxFit.cover, // que la imagen se adapte a la tarjeta
             fadeInDuration:
-                Duration(milliseconds: 300), //duracion del placeholder
+                const Duration(milliseconds: 300), //duracion del placeholder
           ),
-          Container(
-              alignment: AlignmentDirectional
-                  .centerEnd, //posicion del texto de la tarjeta en el container
-              padding: const EdgeInsets.only(
-                  right: 20,
-                  top: 10,
-                  bottom: 10), // el espacio del texto con respecto al container
-              child: const Text('Tunning Car Pinterest'))
+          if (name != null)
+            Container(
+                alignment: AlignmentDirectional
+                    .centerEnd, //posicion del texto de la tarjeta en el container
+                padding: const EdgeInsets.only(
+                    right: 20,
+                    top: 10,
+                    bottom:
+                        10), // el espacio del texto con respecto al container
+                child: Text(name ?? 'sin titulo'))
         ],
       ),
     );
