7826cee89171026922af5edaacdb4b9ecaacccd5
Claudio Plaza <plza0000@gmail.com>
2022-12-22 17:28:49 -0300

    TextFormFields Personalizacion

--- lib/screens/inputs_screen.dart
+++ lib/screens/inputs_screen.dart
@@ -9,8 +9,38 @@ class InputsScreen extends StatelessWidget {
       appBar: AppBar(
         title: const Text('Inputs y Forms'),
       ),
-      body: const Center(
-        child: Text('InputsScreen'),
+      body: SingleChildScrollView(
+        child: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 20),
+          child: Column(
+            children: [
+              TextFormField(
+                  autofocus: true,
+                  initialValue: '',
+                  textCapitalization: TextCapitalization.words,
+                  onChanged: (value) {
+                    print('value: $value');
+                  },
+                  validator: (value) {
+                    if (value == null) return 'Este campo es requerido';
+                    return value.length < 3 ? 'Almenos 3 caracteres' : null;
+                  },
+                  autovalidateMode: AutovalidateMode.onUserInteraction,
+                  decoration: const InputDecoration(
+                      hintText: 'Nombre del Usuario',
+                      labelText: 'Nombre',
+                      helperText: 'Sólo letras',
+                      counterText: '3 caracteres',
+                      //prefixIcon: Icon(Icons.verified_user_outlined),
+                      suffixIcon: Icon(Icons.group),
+                      icon: Icon(Icons.admin_panel_settings_outlined),
+                      border: OutlineInputBorder(
+                          borderRadius: BorderRadius.only(
+                              bottomLeft: Radius.circular(10),
+                              topRight: Radius.circular(10)))))
+            ],
+          ),
+        ),
       ),
     );
   }
